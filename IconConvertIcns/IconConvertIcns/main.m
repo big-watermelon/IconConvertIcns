5d575758e5d913d8fae0a5050d46dd315fa5abe2
big-watermelon <1131448549@qq.com>
2019-01-12 17:44:22 +0800

    1.0
    1.0

---
+++ IconConvertIcns/IconConvertIcns/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  IconConvertIcns
+//
+//  Created by Tenorshare Developer on 2019/1/11.
+//  Copyright © 2019 xxxxxx. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
