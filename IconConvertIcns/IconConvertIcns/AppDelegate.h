c3b8afaa8443ae50f01a53de6c19dc814c36ceb3
big-watermelon <1131448549@qq.com>
2019-01-12 17:14:58 +0800

    1.0版本
    图片转化成Icns

---
+++ IconConvertIcns/IconConvertIcns/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IconConvertIcns
+//
+//  Created by Tenorshare Developer on 2019/1/11.
+//  Copyright © 2019 xxxxxx. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
